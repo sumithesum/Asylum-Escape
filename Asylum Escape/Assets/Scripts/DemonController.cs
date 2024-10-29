db31f100b6255a4cb9ebaa8b515084f69ab93572
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2024-10-29 20:57:42 +0200

    Demon Assets

---
+++ Asylum Escape/Assets/Scripts/DemonController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Exemplu pentru a activa animația de mers
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        // Exemplu pentru a activa animația de atac
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+}
+
