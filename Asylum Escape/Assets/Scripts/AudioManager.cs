18102349060e2e3146720e4af85e39a1b97fc11a
Milan <mujdar.milan@gmail.com>
2024-11-24 16:16:08 +0200

    Sound Effects
    Added AudioManager where we manage all the game sounds, it has a function that can be called from anywhere and will make a new Clip for any new sound we need

---
+++ Asylum Escape/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("--------- Audio Source -----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------- Audio Clip -----------")]
+    public AudioClip background;
+    public AudioClip randomNoise;
+
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+
+}
