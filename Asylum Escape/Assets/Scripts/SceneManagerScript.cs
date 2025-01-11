095c7a216a25a8f986a36668313a4cd1c19a3257
IuLiA109 <iuliatamas03@gmail.com>
2025-01-11 14:51:46 +0200

    menu scene

---
+++ Asylum Escape/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public Image LoadingBarFill;
+
+    public void LoadScene(int sceneId)
+    {
+        //SceneManager.LoadScene(sceneName);
+        LoadingScreen.SetActive(true);
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+
+            LoadingBarFill.fillAmount = progressValue;
+
+            yield return null;
+        }
+    }
+}
